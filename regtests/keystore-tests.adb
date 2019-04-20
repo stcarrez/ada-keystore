081a94b0a69966c76a947bdae908892f80e66ce6
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-04-20 22:10:33 +0200

    Add new tests to check the akt tool - test the help command - test the create command with list, set and get

---
+++ regtests/keystore-tests.adb
@@ -0,0 +1,125 @@
+-----------------------------------------------------------------------
+--  keystore-tests -- Tests for keystore IO
+--  Copyright (C) 2019 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Ada.Text_IO;
+with Ada.Directories;
+with Util.Test_Caller;
+with Util.Encoders.AES;
+with Util.Log.Loggers;
+with Util.Processes;
+with Util.Streams.Buffered;
+with Util.Streams.Pipes;
+package body Keystore.Tests is
+
+   Log : constant Util.Log.Loggers.Logger := Util.Log.Loggers.Create ("Keystore.Tool");
+
+   function Tool return String;
+
+   package Caller is new Util.Test_Caller (Test, "AKT");
+
+   procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite) is
+   begin
+      Caller.Add_Test (Suite, "Test AKT.Commands.Help",
+                       Test_Tool_Help'Access);
+      Caller.Add_Test (Suite, "Test AKT.Commands.Create",
+                       Test_Tool_Create'Access);
+   end Add_Tests;
+
+   --  ------------------------------
+   --  Get the dynamo executable path.
+   --  ------------------------------
+   function Tool return String is
+   begin
+      return "bin/akt";
+   end Tool;
+
+   --  ------------------------------
+   --  Execute the command and get the output in a string.
+   --  ------------------------------
+   procedure Execute (T       : in out Test;
+                      Command : in String;
+                      Result  : out Ada.Strings.Unbounded.Unbounded_String;
+                      Status  : in Natural := 0) is
+      P        : aliased Util.Streams.Pipes.Pipe_Stream;
+      Buffer   : Util.Streams.Buffered.Input_Buffer_Stream;
+   begin
+      Log.Info ("Execute: {0}", Command);
+      P.Open (Command, Util.Processes.READ_ALL);
+
+      --  Write on the process input stream.
+      Result := Ada.Strings.Unbounded.Null_Unbounded_String;
+      Buffer.Initialize (P'Unchecked_Access, 8192);
+      Buffer.Read (Result);
+      P.Close;
+      Ada.Text_IO.Put_Line (Ada.Strings.Unbounded.To_String (Result));
+      Log.Info ("Command result: {0}", Result);
+      Util.Tests.Assert_Equals (T, Status, P.Get_Exit_Status, "Command '" & Command & "' failed");
+   end Execute;
+
+   --  ------------------------------
+   --  Test the akt help command.
+   --  ------------------------------
+   procedure Test_Tool_Help (T : in out Test) is
+      Result : Ada.Strings.Unbounded.Unbounded_String;
+   begin
+      T.Execute (Tool & " help", Result);
+      Util.Tests.Assert_Matches (T, ".*tool to store and protect your sensitive data", Result,
+                                 "Invalid help");
+   end Test_Tool_Help;
+
+   --  ------------------------------
+   --  Test the akt keystore creation.
+   --  ------------------------------
+   procedure Test_Tool_Create (T : in out Test) is
+      Path   : constant String := Util.Tests.Get_Test_Path ("regtests/result/test-tool.akt");
+      Result : Ada.Strings.Unbounded.Unbounded_String;
+   begin
+      if Ada.Directories.Exists (Path) then
+         Ada.Directories.Delete_File (Path);
+      end if;
+
+      --  Create keystore
+      T.Execute (Tool & " -f " & Path & " -p admin create", Result);
+      Util.Tests.Assert_Equals (T, "", Result, "create command failed");
+      T.Assert (Ada.Directories.Exists (Path),
+                "Keystore file does not exist");
+
+      --  List content => empty result
+      T.Execute (Tool & " -f " & Path & " -p admin list", Result);
+      Util.Tests.Assert_Equals (T, "", Result, "list command failed");
+
+      --  Set property
+      T.Execute (Tool & " -f " & Path & " -p admin set testing my-testing-value", Result);
+      Util.Tests.Assert_Equals (T, "", Result, "set command failed");
+
+      --  Get property
+      T.Execute (Tool & " -f " & Path & " -p admin get testing", Result);
+      Util.Tests.Assert_Matches (T, "^my-testing-value", Result, "get command failed");
+
+      --  List content => one entry
+      T.Execute (Tool & " -f " & Path & " -p admin list", Result);
+      Util.Tests.Assert_Matches (T, "^testing", Result, "list command failed");
+
+      --  Open keystore with invalid password
+      T.Execute (Tool & " -f " & Path & " -p admin2 list", Result, 1);
+      Util.Tests.Assert_Matches (T, "^ERROR: Invalid password to unlock the keystore file",
+                                 Result, "list command failed");
+
+   end Test_Tool_Create;
+
+end Keystore.Tests;
