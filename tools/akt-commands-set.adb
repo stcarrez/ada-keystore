ab6ec298ad5aced4d6a0adbe2ff136ea27302796
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-04-20 17:33:17 +0200

    Implement the set command to set a content in the keystore

---
+++ tools/akt-commands-set.adb
@@ -0,0 +1,53 @@
+-----------------------------------------------------------------------
+--  akt-commands-set -- Set content in keystore
+--  Copyright (C) 2019 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Text_IO;
+package body AKT.Commands.Set is
+
+   --  ------------------------------
+   --  Insert a new value in the keystore.
+   --  ------------------------------
+   overriding
+   procedure Execute (Command   : in out Command_Type;
+                      Name      : in String;
+                      Args      : in Argument_List'Class;
+                      Context   : in out Context_Type) is
+   begin
+      if Args.Get_Count /= 2 then
+         AKT.Commands.Usage (Context, Args);
+      else
+         Context.Open_Keystore;
+         Context.Wallet.Add (Name    => Args.Get_Argument (1),
+                             Content => Args.Get_Argument (2));
+      end if;
+   end Execute;
+
+   --  ------------------------------
+   --  Write the help associated with the command.
+   --  ------------------------------
+   overriding
+   procedure Help (Command   : in out Command_Type;
+                   Context   : in out Context_Type) is
+   begin
+      Ada.Text_IO.Put_Line ("set: insert a new value in the keystore");
+      Ada.Text_IO.New_Line;
+      Ada.Text_IO.Put_Line ("Usage: set <name> <value>");
+      Ada.Text_IO.New_Line;
+      Ada.Text_IO.Put_Line ("  ");
+   end Help;
+
+end AKT.Commands.Set;
