0a0f48a1ada5a188f16d3351a57bbbeb11fb6e7e
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-04-20 17:20:28 +0200

    New password provider that reads a file to get the password

---
+++ tools/akt-passwords-files.ads
@@ -0,0 +1,24 @@
+-----------------------------------------------------------------------
+--  akt-passwords-files -- File based password provider
+--  Copyright (C) 2019 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+package AKT.Passwords.Files is
+
+   --  Create a password provider that reads the file to build the password.
+   function Create (Path : in String) return Provider_Access;
+
+end AKT.Passwords.Files;
