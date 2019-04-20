2562c0e77629b112c1b086d85fdae6cdbde069f6
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-04-20 17:32:54 +0200

    New package for the definition of the set command

---
+++ tools/akt-commands-set.ads
@@ -0,0 +1,35 @@
+-----------------------------------------------------------------------
+--  akt-commands-set -- Set content in keystore
+--  Copyright (C) 2019 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with AKT.Commands.Drivers;
+private package AKT.Commands.Set is
+
+   type Command_Type is new AKT.Commands.Drivers.Command_Type with null record;
+
+   --  Insert a new value in the keystore.
+   overriding
+   procedure Execute (Command   : in out Command_Type;
+                      Name      : in String;
+                      Args      : in Argument_List'Class;
+                      Context   : in out Context_Type);
+
+   --  Write the help associated with the command.
+   overriding
+   procedure Help (Command   : in out Command_Type;
+                   Context   : in out Context_Type);
+
+end AKT.Commands.Set;
