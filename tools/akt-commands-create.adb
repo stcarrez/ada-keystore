92011576075ee69a19d9e52935c1e98a818ed9ae
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-04-20 17:31:59 +0200

    Implement the create keystore command

---
+++ tools/akt-commands-create.adb
@@ -0,0 +1,48 @@
+-----------------------------------------------------------------------
+--  akt-commands-create -- Create a keystore
+--  Copyright (C) 2019 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Text_IO;
+package body AKT.Commands.Create is
+
+   --  ------------------------------
+   --  Create the keystore file.
+   --  ------------------------------
+   overriding
+   procedure Execute (Command   : in out Command_Type;
+                      Name      : in String;
+                      Args      : in Argument_List'Class;
+                      Context   : in out Context_Type) is
+      pragma Unreferenced (Command, Name, Args);
+   begin
+      Keystore.Files.Create (Container => Context.Wallet,
+                             Password  => Context.Provider.Get_Password,
+                             Path      => Context.Wallet_File.all);
+   end Execute;
+
+   --  ------------------------------
+   --  Write the help associated with the command.
+   --  ------------------------------
+   overriding
+   procedure Help (Command   : in out Command_Type;
+                   Context   : in out Context_Type) is
+   begin
+      Ada.Text_IO.Put_Line ("create: create the keystore");
+      Ada.Text_IO.New_Line;
+      Command.Usage ("create", Context);
+   end Help;
+
+end AKT.Commands.Create;
