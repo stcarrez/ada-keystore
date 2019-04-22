6fd1be5bb2ff8b2c787ddb4d2fe2c8b7662a75dc
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-04-22 20:26:32 +0200

    Declare Test_Update procedure to check updating values in the keystore

--- regtests/keystore-files-tests.ads
+++ regtests/keystore-files-tests.ads
@@ -38,6 +38,9 @@ package Keystore.Files.Tests is
    --  Test deleting values.
    procedure Test_Delete (T : in out Test);
 
+   --  Test update values.
+   procedure Test_Update (T : in out Test);
+
    --  Test opening and closing keystore.
    procedure Test_Open_Close (T : in out Test);
 
