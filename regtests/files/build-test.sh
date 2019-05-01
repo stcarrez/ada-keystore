694f34e19c1233bc2dec7fdb3f742a8399b0aafc
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-05-01 10:22:25 +0200

    Update the script to list the keystore content

--- regtests/files/build-test.sh
+++ regtests/files/build-test.sh
@@ -1,5 +1,7 @@
+rm -f regtests/files/test-keystore.ks
 bin/akt -f regtests/files/test-keystore.ks -p mypassword create
 bin/akt -f regtests/files/test-keystore.ks -p mypassword set list-1 'Licensed under the Apache License, Version 2.0 (the "License");'
 bin/akt -f regtests/files/test-keystore.ks -p mypassword set list-2 'you may not use this file except in compliance with the License.'
 bin/akt -f regtests/files/test-keystore.ks -p mypassword set list-3 'You may obtain a copy of the License at'
 bin/akt -f regtests/files/test-keystore.ks -p mypassword set list-4 'http://www.apache.org/licenses/LICENSE-2.0'
+bin/akt -f regtests/files/test-keystore.ks -p mypassword list
