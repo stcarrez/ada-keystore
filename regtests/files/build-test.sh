f60f6fb78fbcd4690cafc78af9e2698b7cc731b7
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-05-04 16:30:07 +0200

    Update the script to insert a value that fits in several data blocks

--- regtests/files/build-test.sh
+++ regtests/files/build-test.sh
@@ -4,4 +4,5 @@ bin/akt -f regtests/files/test-keystore.ks -p mypassword set list-1 'Licensed un
 bin/akt -f regtests/files/test-keystore.ks -p mypassword set list-2 'you may not use this file except in compliance with the License.'
 bin/akt -f regtests/files/test-keystore.ks -p mypassword set list-3 'You may obtain a copy of the License at'
 bin/akt -f regtests/files/test-keystore.ks -p mypassword set list-4 'http://www.apache.org/licenses/LICENSE-2.0'
+bin/akt -f regtests/files/test-keystore.ks -p mypassword set LICENSE.txt -f LICENSE.txt
 bin/akt -f regtests/files/test-keystore.ks -p mypassword list
