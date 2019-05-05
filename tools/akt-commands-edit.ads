e9bf28af0118c391f3236f181eb8397bb4ba21da
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-05-05 09:52:19 +0200

    New package for the implementation of the edit command

---
+++ tools/akt-commands-edit.ads
@@ -0,0 +1,54 @@
+-----------------------------------------------------------------------
+--  akt-commands-edit -- Edit content in keystore
+--  Copyright (C) 2019 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with AKT.Commands.Drivers;
+private package AKT.Commands.Edit is
+
+   type Command_Type is new AKT.Commands.Drivers.Command_Type with private;
+
+   --  Get the editor command to launch.
+   function Get_Editor (Command : in Command_Type) return String;
+
+   --  Get the directory where the editor's file can be created.
+   function Get_Directory (Command : in Command_Type;
+                           Context : in out Context_Type) return String;
+
+   --  Edit a value from the keystore by using an external editor.
+   overriding
+   procedure Execute (Command   : in out Command_Type;
+                      Name      : in String;
+                      Args      : in Argument_List'Class;
+                      Context   : in out Context_Type);
+
+   --  Setup the command before parsing the arguments and executing it.
+   overriding
+   procedure Setup (Command : in out Command_Type;
+                    Config  : in out GNAT.Command_Line.Command_Line_Configuration;
+                    Context : in out Context_Type);
+
+   --  Write the help associated with the command.
+   overriding
+   procedure Help (Command   : in out Command_Type;
+                   Context   : in out Context_Type);
+
+private
+
+   type Command_Type is new AKT.Commands.Drivers.Command_Type with record
+      Editor : aliased GNAT.Strings.String_Access;
+   end record;
+
+end AKT.Commands.Edit;
