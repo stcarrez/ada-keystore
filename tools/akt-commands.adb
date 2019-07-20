f358eb482c9dfb25776322a48e445b9dbed6fc66
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-07-20 23:24:48 +0200

    Update the Change_Password procedure to allow removing a key slot Define and register the password clear and add

--- tools/akt-commands.adb
+++ tools/akt-commands.adb
@@ -30,6 +30,7 @@ with AKT.Commands.Remove;
 with AKT.Commands.Edit;
 with AKT.Commands.Store;
 with AKT.Commands.Password;
+with AKT.Commands.Password.Remove;
 with AKT.Passwords.Input;
 with AKT.Passwords.Files;
 with AKT.Passwords.Unsafe;
@@ -45,6 +46,8 @@ package body AKT.Commands is
    Remove_Command   : aliased AKT.Commands.Remove.Command_Type;
    Edit_Command     : aliased AKT.Commands.Edit.Command_Type;
    Change_Password_Command : aliased AKT.Commands.Password.Command_Type;
+   Add_Password_Command    : aliased AKT.Commands.Password.Add_Command_Type;
+   Del_Password_Command    : aliased AKT.Commands.Password.Remove.Command_Type;
 
    Driver           : Drivers.Driver_Type;
    Arguments        : Util.Commands.Dynamic_Argument_List;
@@ -95,14 +98,14 @@ package body AKT.Commands is
    --  ------------------------------
    procedure Change_Password (Context      : in out Context_Type;
                               New_Password : in Keystore.Secret_Key;
-                              Slot         : in Keystore.Key_Slot_Index) is
+                              Mode         : in Keystore.Mode_Type) is
       Password : constant Keystore.Secret_Key := Context.Provider.Get_Password;
    begin
       Context.Wallet.Open (Password => Password,
                            Path     => Context.Wallet_File.all);
       Context.Wallet.Set_Key (Password     => Password,
                               New_Password => New_Password,
-                              Slot         => Slot);
+                              Mode         => Mode);
    end Change_Password;
 
    --  ------------------------------
@@ -209,8 +212,9 @@ package body AKT.Commands is
       Driver.Add_Command ("list", "list values of the keystore", List_Command'Access);
       Driver.Add_Command ("remove", "remove values from the keystore", Remove_Command'Access);
       Driver.Add_Command ("edit", "edit the value with an external editor", Edit_Command'Access);
-      Driver.Add_Command ("change-password", "change the password", Change_Password_Command'Access);
-      Driver.Add_Command ("add-password", "add a password", Change_Password_Command'Access);
+      Driver.Add_Command ("password-set", "change the password", Change_Password_Command'Access);
+      Driver.Add_Command ("password-add", "add a password", Add_Password_Command'Access);
+      Driver.Add_Command ("password-clear", "remove a password", Del_Password_Command'Access);
    end Initialize;
 
    procedure Parse (Context   : in out Context_Type;
