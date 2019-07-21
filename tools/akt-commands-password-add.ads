dab731ee4818268ece8bde080969f3ae04cfe1fb
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-07-21 10:30:29 +0200

    Refactor password command

---
+++ tools/akt-commands-password-add.ads
@@ -0,0 +1,38 @@
+-----------------------------------------------------------------------
+--  akt-commands-password-add -- Add a wallet password
+--  Copyright (C) 2019 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+package AKT.Commands.Password.Add is
+
+   type Command_Type is new AKT.Commands.Password.Command_Type with private;
+
+   --  Add a wallet password.
+   overriding
+   procedure Execute (Command   : in out Command_Type;
+                      Name      : in String;
+                      Args      : in Argument_List'Class;
+                      Context   : in out Context_Type);
+
+   --  Write the help associated with the command.
+   overriding
+   procedure Help (Command   : in out Command_Type;
+                   Context   : in out Context_Type);
+
+private
+
+   type Command_Type is new AKT.Commands.Password.Command_Type with null record;
+
+end AKT.Commands.Password.Add;
