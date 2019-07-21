48f313233d5dc2f305b07c5c1830d3ba14f071da
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-07-21 14:01:13 +0200

    Remove unused with Ada.Text_IO clause

--- tools/akt-commands-password.adb
+++ tools/akt-commands-password.adb
@@ -15,7 +15,6 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-with Ada.Text_IO;
 with AKT.Passwords.Files;
 with AKT.Passwords.Unsafe;
 with AKT.Passwords.Input;
