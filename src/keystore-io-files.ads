00345a7705c3636b3963bd0504fa714eb6d24043
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-08-18 22:50:11 +0200

    Refactor the IO operations - add support for multiple files to store the data in a keystore - use Storage_Block type to identify the data block to read/write

--- src/keystore-io-files.ads
+++ src/keystore-io-files.ads
@@ -15,27 +15,71 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-with Ada.Direct_IO;
 with Ada.Containers.Ordered_Sets;
+with Ada.Containers.Hashed_Maps;
+with Util.Streams.Raw;
+private with Util.Systems.Types;
+private with Ada.Finalization;
 package Keystore.IO.Files is
+
+   type Wallet_Stream is limited new Keystore.IO.Wallet_Stream with private;
+   type Wallet_Stream_Access is access all Wallet_Stream'Class;
+
+   --  Open the wallet stream.
+   procedure Open (Stream : in out Wallet_Stream;
+                   Path   : in String);
+
+   procedure Create (Stream : in out Wallet_Stream;
+                     Path   : in String;
+                     Config : in Wallet_Config);
+
+   --  Read from the wallet stream the block identified by the number and
+   --  call the `Process` procedure with the data block content.
+   overriding
+   procedure Read (Stream  : in out Wallet_Stream;
+                   Block   : in Storage_Block;
+                   Process : not null access
+                     procedure (Data : in Block_Type));
+
+   --  Write in the wallet stream the block identified by the block number.
+   overriding
+   procedure Write (Stream  : in out Wallet_Stream;
+                    Block   : in Storage_Block;
+                    Process : not null access
+                      procedure (Data : out Block_Type));
+
+   --  Allocate a new block and return the block number in `Block`.
+   overriding
+   procedure Allocate (Stream  : in out Wallet_Stream;
+                       Kind    : in Block_Kind;
+                       Block   : out Storage_Block);
+
+   --  Release the block number.
+   overriding
+   procedure Release (Stream  : in out Wallet_Stream;
+                      Block   : in Storage_Block);
+
+   overriding
+   function Is_Used (Stream  : in out Wallet_Stream;
+                     Block   : in Storage_Block) return Boolean;
+
+   --  Close the wallet stream and release any resource.
+   procedure Close (Stream : in out Wallet_Stream);
+
+private
+
+   use type Block_Number;
+   use type Storage_Identifier;
 
    package Block_Number_Sets is
      new Ada.Containers.Ordered_Sets (Element_Type => Block_Number,
                                       "<"          => "<",
                                       "="          => "=");
 
-   package Block_IO is
-      new Ada.Direct_IO (Element_Type => Keystore.IO.Block_Type);
+   protected type File_Stream is
 
-   use type Block_IO.Positive_Count;
-
-   subtype Positive_Count is Block_IO.Positive_Count;
-
-   protected type Wallet_File_Stream is new Keystore.IO.Wallet_Stream with
-
-      procedure Open (Path : in String);
-
-      procedure Create (Path : in String);
+      procedure Open (File_Descriptor : in Util.Systems.Types.File_Type;
+                      File_Size       : in Block_Count);
 
       --  Read from the wallet stream the block identified by the number and
       --  call the `Process` procedure with the data block content.
@@ -48,25 +92,59 @@ package Keystore.IO.Files is
                        Process : not null access
                          procedure (Data : out Block_Type));
 
-      --  Returns true if the block number is allocated.
-      function Is_Used (Block  : in Block_Number) return Boolean;
-
       --  Allocate a new block and return the block number in `Block`.
-      procedure Allocate (Block  : out Block_Number);
+      procedure Allocate (Block   : out Block_Number);
 
       --  Release the block number.
-      procedure Release (Block  : in Block_Number);
+      procedure Release (Block   : in Block_Number);
 
-      --  Close the keystore file.
+      function Is_Used (Block : in Block_Number) return Boolean;
+
       procedure Close;
 
    private
-      File        : Block_IO.File_Type;
-      Size        : Block_IO.Count;
-      Data        : Marshaller;
+      File        : Util.Streams.Raw.Raw_Stream;
+      Current_Pos : Util.Systems.Types.off_t;
+      Size        : Block_Count;
+      Data        : Block_Type;
       Free_Blocks : Block_Number_Sets.Set;
-   end Wallet_File_Stream;
+   end File_Stream;
 
-   type Wallet_File_Stream_Access is access all Wallet_File_Stream;
+   type File_Stream_Access is access all File_Stream;
+
+   function Hash (Value : Storage_Identifier) return Ada.Containers.Hash_Type;
+
+   package File_Stream_Maps is
+     new Ada.Containers.Hashed_Maps (Key_Type        => Storage_Identifier,
+                                     Element_Type    => File_Stream_Access,
+                                     Hash            => Hash,
+                                     Equivalent_Keys => "=",
+                                     "="             => "=");
+
+   protected type Stream_Descriptor is
+
+      procedure Open (Path : in String);
+
+      procedure Create (Path   : in String;
+                        Config : in Wallet_Config);
+
+      procedure Get (Storage : in Storage_Identifier;
+                     File    : out File_Stream_Access);
+
+      procedure Allocate (Kind    : in Block_Kind;
+                          Storage : out Storage_Identifier;
+                          File    : out File_Stream_Access);
+
+      procedure Close;
+
+   private
+      UUID  : UUID_Type;
+      Files : File_Stream_Maps.Map;
+   end Stream_Descriptor;
+
+   type Wallet_Stream is limited new Ada.Finalization.Limited_Controlled
+     and Keystore.IO.Wallet_Stream with record
+      Descriptor : Stream_Descriptor;
+   end record;
 
 end Keystore.IO.Files;
