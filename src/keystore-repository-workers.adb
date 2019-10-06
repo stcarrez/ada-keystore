576736231abe1bdc0e7b00880f2f72961079d8e1
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-10-06 18:08:51 +0200

    Update after changes in marshaller operations

--- src/keystore-repository-workers.adb
+++ src/keystore-repository-workers.adb
@@ -199,7 +199,7 @@ package body Keystore.Repository.Workers is
                         Decrypt_Size => Size,
                         Into         => Data_Block.Buffer);
 
-      Data_Block.Pos := IO.BT_HEADER_START;
+      Data_Block.Pos := IO.BT_HEADER_START - 1;
 
       --  Check block type.
       Btype := Marshallers.Get_Unsigned_16 (Data_Block);
