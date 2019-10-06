53379974c91561da89d80a2150b5ca5f4624a1bc
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-10-06 20:37:31 +0200

    Declare On_Tool_Lock procedure

--- gtk/akt-callbacks.ads
+++ gtk/akt-callbacks.ads
@@ -42,6 +42,9 @@ package AKT.Callbacks is
    --  Callback executed when the "tool-edit" action is executed from the toolbar.
    procedure On_Tool_Edit (Object : access Gtkada.Builder.Gtkada_Builder_Record'Class);
 
+   --  Callback executed when the "tool-lock" action is executed from the toolbar.
+   procedure On_Tool_Lock (Object : access Gtkada.Builder.Gtkada_Builder_Record'Class);
+
    --  Callback executed when the "open-file" action is executed from the open_file dialog.
    procedure On_Open_File (Object : access Gtkada.Builder.Gtkada_Builder_Record'Class);
 
