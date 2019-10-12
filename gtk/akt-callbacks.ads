015c5aedb90d18ed98dca744b458b82f30775563
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-10-12 17:31:04 +0200

    Declare On_Create_File and On_Cancel_Create_File

--- gtk/akt-callbacks.ads
+++ gtk/akt-callbacks.ads
@@ -30,8 +30,8 @@ package AKT.Callbacks is
    --  Callback executed when the "about" action is executed from the menu.
    procedure On_Menu_About (Object : access Gtkada.Builder.Gtkada_Builder_Record'Class);
 
-   --  Callback executed when the "menu-create" action is executed from the file menu.
-   procedure On_Menu_Create (Object : access Gtkada.Builder.Gtkada_Builder_Record'Class);
+   --  Callback executed when the "menu-new" action is executed from the file menu.
+   procedure On_Menu_New (Object : access Gtkada.Builder.Gtkada_Builder_Record'Class);
 
    --  Callback executed when the "menu-open" action is executed from the file menu.
    procedure On_Menu_Open (Object : access Gtkada.Builder.Gtkada_Builder_Record'Class);
@@ -53,6 +53,13 @@ package AKT.Callbacks is
 
    --  Callback executed when the "cancel-open-file" action is executed from the open_file dialog.
    procedure On_Cancel_Open_File (Object : access Gtkada.Builder.Gtkada_Builder_Record'Class);
+
+   --  Callback executed when the "create-file" action is executed from the open_file dialog.
+   procedure On_Create_File (Object : access Gtkada.Builder.Gtkada_Builder_Record'Class);
+
+   --  Callback executed when the "cancel-create-file" action is executed
+   --  from the open_file dialog.
+   procedure On_Cancel_Create_File (Object : access Gtkada.Builder.Gtkada_Builder_Record'Class);
 
    --  Callback executed when the "delete-event" action is executed from the main window.
    function On_Close_Window (Object : access Gtkada.Builder.Gtkada_Builder_Record'Class)
