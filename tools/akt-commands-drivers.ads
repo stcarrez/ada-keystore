2cf01c249e4a93d6b3caf5b909572c2160a19444
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-10-26 22:56:09 +0200

    Refactor to have a common Command_Type base type with a generic Help procedure

--- tools/akt-commands-drivers.ads
+++ tools/akt-commands-drivers.ads
@@ -18,6 +18,27 @@
 with Util.Commands.Drivers;
 with Util.Commands.Parsers.GNAT_Parser;
 private package AKT.Commands.Drivers is
-  new Util.Commands.Drivers (Context_Type  => Context_Type,
-                             Config_Parser => Util.Commands.Parsers.GNAT_Parser.Config_Parser,
-                             Driver_Name   => "akt");
+
+   package Main_Driver is
+     new Util.Commands.Drivers (Context_Type  => Context_Type,
+                                Config_Parser => Util.Commands.Parsers.GNAT_Parser.Config_Parser,
+                                Driver_Name   => "akt");
+
+   subtype Help_Command_Type is Main_Driver.Help_Command_Type;
+   subtype Driver_Type is Main_Driver.Driver_Type;
+
+   type Command_Type is abstract new Main_Driver.Command_Type with null record;
+
+   --  Setup the command before parsing the arguments and executing it.
+   overriding
+   procedure Setup (Command : in out Command_Type;
+                    Config  : in out GNAT.Command_Line.Command_Line_Configuration;
+                    Context : in out Context_Type);
+
+   --  Write the help associated with the command.
+   overriding
+   procedure Help (Command   : in out Command_Type;
+                   Name      : in String;
+                   Context   : in out Context_Type);
+
+end AKT.Commands.Drivers;
