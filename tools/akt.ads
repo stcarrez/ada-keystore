328c43e36dec2fbfb3b45c45b2837afb7fe9d886
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-11-03 15:22:44 +0100

    Declare "-" for i18n

--- tools/akt.ads
+++ tools/akt.ads
@@ -15,8 +15,10 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-
+with Intl;
 package AKT is
+
+   function "-" (Message : in String) return String is (Intl."-" (Message));
 
 private
 
