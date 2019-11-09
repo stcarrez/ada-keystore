7030aa0d22e6780931fd080f27d18572a6dee937
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-11-09 22:53:59 +0100

    Update the script

--- regtests/files/build-test.sh
+++ regtests/files/build-test.sh
@@ -1,9 +1,9 @@
 rm -f regtests/files/test-keystore.akt
 # Use a low counter range to speed up the tests
-bin/akt -f regtests/files/test-keystore.akt -p mypassword create --counter-range 1:1000 --force
-bin/akt -f regtests/files/test-keystore.akt -p mypassword set list-1 'Licensed under the Apache License, Version 2.0 (the "License");'
-bin/akt -f regtests/files/test-keystore.akt -p mypassword set list-2 'you may not use this file except in compliance with the License.'
-bin/akt -f regtests/files/test-keystore.akt -p mypassword set list-3 'You may obtain a copy of the License at'
-bin/akt -f regtests/files/test-keystore.akt -p mypassword set list-4 'http://www.apache.org/licenses/LICENSE-2.0'
-bin/akt -f regtests/files/test-keystore.akt -p mypassword set LICENSE.txt -f LICENSE.txt
-bin/akt -f regtests/files/test-keystore.akt -p mypassword list
+bin/akt create -k regtests/files/test-keystore.akt -p mypassword --counter-range 1:1000 --force
+bin/akt set -k regtests/files/test-keystore.akt -p mypassword list-1 'Licensed under the Apache License, Version 2.0 (the "License");'
+bin/akt set -k regtests/files/test-keystore.akt -p mypassword list-2 'you may not use this file except in compliance with the License.'
+bin/akt set -k regtests/files/test-keystore.akt -p mypassword list-3 'You may obtain a copy of the License at'
+bin/akt set -k regtests/files/test-keystore.akt -p mypassword list-4 'http://www.apache.org/licenses/LICENSE-2.0'
+bin/akt set -k regtests/files/test-keystore.akt -p mypassword -f LICENSE.txt
+bin/akt list -k regtests/files/test-keystore.akt -p mypassword
