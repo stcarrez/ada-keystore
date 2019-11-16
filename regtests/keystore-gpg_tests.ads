0f3e7165ac3efbce54a03310feb8d842d8efb0eb
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-11-16 18:13:06 +0100

    New package to define and run unit tests with GPG protection

---
+++ regtests/keystore-gpg_tests.ads
@@ -0,0 +1,40 @@
+-----------------------------------------------------------------------
+--  keystore-gpg_tests -- Test AKT with GPG2
+--  Copyright (C) 2019 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Ada.Strings.Unbounded;
+with Util.Tests;
+package Keystore.GPG_Tests is
+
+   procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite);
+
+   type Test is new Util.Tests.Test with null record;
+
+   --  Test the akt keystore creation.
+   procedure Test_Create (T : in out Test);
+
+   procedure Execute (T       : in out Test;
+                      Command : in String;
+                      Result  : out Ada.Strings.Unbounded.Unbounded_String;
+                      Status  : in Natural := 0);
+
+   procedure Execute (T       : in out Test;
+                      Command : in String;
+                      Expect  : in String;
+                      Status  : in Natural := 0);
+
+end Keystore.GPG_Tests;
