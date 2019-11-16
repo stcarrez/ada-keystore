9b0d58ba916a0a8ef01dc54761bc519dd46aed7b
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-11-16 18:45:40 +0100

    New test Test_Create_Multi_User to check creation of keystore with 3 GPG user keys

--- regtests/keystore-gpg_tests.ads
+++ regtests/keystore-gpg_tests.ads
@@ -27,6 +27,9 @@ package Keystore.GPG_Tests is
    --  Test the akt keystore creation.
    procedure Test_Create (T : in out Test);
 
+   --  Test the akt keystore for several users each having their own GPG key.
+   procedure Test_Create_Multi_User (T : in out Test);
+
    procedure Execute (T       : in out Test;
                       Command : in String;
                       Result  : out Ada.Strings.Unbounded.Unbounded_String;
