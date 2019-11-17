68bc67305e6efe65b019977695e4c83eaf274f0e
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-11-17 10:35:45 +0100

    Cleanup and remove unused options and Setup procedure

--- tools/akt-commands-config.ads
+++ tools/akt-commands-config.ads
@@ -18,26 +18,12 @@
 with AKT.Commands.Drivers;
 private package AKT.Commands.Config is
 
-   type Command_Type is new AKT.Commands.Drivers.Command_Type with private;
+   type Command_Type is new AKT.Commands.Drivers.Command_Type with null record;
 
    overriding
    procedure Execute (Command   : in out Command_Type;
                       Name      : in String;
                       Args      : in Argument_List'Class;
                       Context   : in out Context_Type);
-
-   --  Setup the command before parsing the arguments and executing it.
-   overriding
-   procedure Setup (Command : in out Command_Type;
-                    Config  : in out GNAT.Command_Line.Command_Line_Configuration;
-                    Context : in out Context_Type);
-
-private
-
-   type Command_Type is new AKT.Commands.Drivers.Command_Type with record
-      Output     : aliased GNAT.Strings.String_Access;
-      Dir        : aliased GNAT.Strings.String_Access;
-      No_Newline : aliased Boolean := False;
-   end record;
 
 end AKT.Commands.Config;
