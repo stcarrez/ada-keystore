942cfe598f7350199bb3f47e740555a2722a7f33
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-11-22 23:09:44 +0100

    Refactor the commands: change set command to only set from the command arguments

--- tools/akt-commands-set.adb
+++ tools/akt-commands-set.adb
@@ -15,14 +15,8 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-with Ada.Text_IO;
-with Ada.Directories;
-with Ada.Streams.Stream_IO;
-with Util.Streams.Files;
-with Keystore.Tools;
-package body AKT.Commands.Set is
 
-   use GNAT.Strings;
+package body AKT.Commands.Set is
 
    --  ------------------------------
    --  Insert a new value in the keystore.
@@ -32,75 +26,17 @@ package body AKT.Commands.Set is
                       Name      : in String;
                       Args      : in Argument_List'Class;
                       Context   : in out Context_Type) is
-      function Accept_File (Ent : in Keystore.Tools.Directory_Entry_Type) return Boolean;
-
-      function Accept_File (Ent : in Keystore.Tools.Directory_Entry_Type) return Boolean is
-      begin
-         Ada.Text_IO.Put_Line (Ada.Directories.Full_Name (Ent));
-         return True;
-      end Accept_File;
-
+      pragma Unreferenced (Command);
    begin
-      if Command.Dir /= null and then Command.Dir'Length > 0 then
-         if Args.Get_Count /= 0 then
-            AKT.Commands.Usage (Args, Context, Name);
-            return;
-         end if;
-
-         --  Open keystore with workers because we expect possibly big data.
-         Context.Open_Keystore (Use_Worker => True);
-         Keystore.Tools.Store (Wallet  => Context.Wallet,
-                               Path    => Command.Dir.all,
-                               Prefix  => Ada.Directories.Simple_Name (Command.Dir.all) & '/',
-                               Pattern => "*",
-                               Filter  => Accept_File'Access);
-
-      elsif Command.File /= null and then Command.File'Length > 0 then
-         declare
-            File    : Util.Streams.Files.File_Stream;
-         begin
-            File.Open (Mode => Ada.Streams.Stream_IO.In_File,
-                       Name => Command.File.all);
-
-            --  Open keystore with workers because we expect possibly big data.
-            Context.Open_Keystore (Use_Worker => True);
-            if Args.Get_Count = 1 then
-               Context.Wallet.Set (Name  => Args.Get_Argument (1),
-                                   Kind  => Keystore.T_STRING,
-                                   Input => File);
-            elsif Args.Get_Count = 0 then
-               Context.Wallet.Set (Name  => Ada.Directories.Simple_Name (Command.File.all),
-                                   Kind  => Keystore.T_STRING,
-                                   Input => File);
-            else
-               AKT.Commands.Usage (Args, Context, Name);
-            end if;
-         end;
-
-      elsif Args.Get_Count /= 2 then
+      --  Open keystore without use workers because we expect small data.
+      Context.Open_Keystore (Args, Use_Worker => False);
+      if Args.Get_Count /= Context.First_Arg + 1 then
          AKT.Commands.Usage (Args, Context, Name);
 
       else
-         --  Open keystore without use workers because we expect small data.
-         Context.Open_Keystore (Use_Worker => False);
-         Context.Wallet.Set (Name    => Args.Get_Argument (1),
-                             Content => Args.Get_Argument (2));
+         Context.Wallet.Set (Name    => Args.Get_Argument (Context.First_Arg),
+                             Content => Args.Get_Argument (Context.First_Arg + 1));
       end if;
    end Execute;
-
-   --  ------------------------------
-   --  Setup the command before parsing the arguments and executing it.
-   --  ------------------------------
-   procedure Setup (Command : in out Command_Type;
-                    Config  : in out GNAT.Command_Line.Command_Line_Configuration;
-                    Context : in out Context_Type) is
-      package GC renames GNAT.Command_Line;
-   begin
-      Drivers.Command_Type (Command).Setup (Config, Context);
-      GC.Define_Switch (Config, Command.File'Access,
-                        "-f:", "--file=", -("Define the path of the file to read"));
-      GC.Define_Switch (Config, Command.Dir'Access,
-                        "-r:", "--recursive=", -("Read and store files recursively"));
-   end Setup;
 
 end AKT.Commands.Set;
