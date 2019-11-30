27235a5bdb3e3d4e163f2f280923913e0c934edb
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-11-30 17:41:43 +0100

    Update the script

--- regtests/files/build-test.sh
+++ regtests/files/build-test.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 rm -f regtests/files/test-keystore.akt
 # Use a low counter range to speed up the tests
 bin/akt create -k regtests/files/test-keystore.akt -p mypassword --counter-range 1:1000 --force
@@ -5,5 +6,5 @@ bin/akt set -k regtests/files/test-keystore.akt -p mypassword list-1 'Licensed u
 bin/akt set -k regtests/files/test-keystore.akt -p mypassword list-2 'you may not use this file except in compliance with the License.'
 bin/akt set -k regtests/files/test-keystore.akt -p mypassword list-3 'You may obtain a copy of the License at'
 bin/akt set -k regtests/files/test-keystore.akt -p mypassword list-4 'http://www.apache.org/licenses/LICENSE-2.0'
-bin/akt set -k regtests/files/test-keystore.akt -p mypassword -f LICENSE.txt
+bin/akt store -k regtests/files/test-keystore.akt -p mypassword LICENSE.txt
 bin/akt list -k regtests/files/test-keystore.akt -p mypassword
