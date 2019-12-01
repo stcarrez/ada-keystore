eb6baac0012543390252f8ec28f35fe113d2c07a
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-12-01 10:36:09 +0100

    Add Create and Generate function to load and generate a key file available through the Key_Provider interface

--- src/keystore-passwords-files.ads
+++ src/keystore-passwords-files.ads
@@ -15,10 +15,27 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-
+with Keystore.Passwords.Keys;
 package Keystore.Passwords.Files is
 
+   MAX_FILE_SIZE           : constant := 1024;
+   DEFAULT_KEY_FILE_LENGTH : constant := 32 + 16 + 32;
+
    --  Create a password provider that reads the file to build the password.
+   --  The file must have the mode rw------- (600) and its owning directory
+   --  the mode rwx------ (700).  The Bad_Password exception is raised if
+   --  these rules are not verified.
    function Create (Path : in String) return Provider_Access;
+
+   --  Create a key provider that reads the file.  The file is split in three parts
+   --  the key, the IV, the signature which are extracted by using `Get_Keys`.
+   function Create (Path : in String) return Keys.Key_Provider_Access;
+
+   --  Generate a file that contains the keys.  Keys are generated using a random generator.
+   --  The file is created with the mode rw------- (600) and the owning directory is forced
+   --  to the mode rwx------ (700).
+   function Generate (Path   : in String;
+                      Length : in Key_Length := DEFAULT_KEY_FILE_LENGTH)
+                      return Keys.Key_Provider_Access;
 
 end Keystore.Passwords.Files;
