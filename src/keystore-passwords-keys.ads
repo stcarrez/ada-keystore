110c45854b33456f0853babe4e643936e6108883
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-12-01 21:25:57 +0100

    Declare the Create function to build a key provider from a string or array of bytes

--- src/keystore-passwords-keys.ads
+++ src/keystore-passwords-keys.ads
@@ -20,6 +20,8 @@ package Keystore.Passwords.Keys is
 
    use type Ada.Streams.Stream_Element_Offset;
 
+   DEFAULT_KEY_LENGTH : constant := 32 + 16 + 32;
+
    type Key_Provider is limited interface;
    type Key_Provider_Access is access all Key_Provider'Class;
 
@@ -29,5 +31,12 @@ package Keystore.Passwords.Keys is
                        IV   : out Secret_Key;
                        Sign : out Secret_Key) is abstract with
      Post'Class => Key.Length = 32 and IV.Length = 16 and Sign.Length = 32;
+
+   --  Create a key, iv, sign provider from the string.
+   function Create (Password : in String;
+                    Length   : in Key_Length := DEFAULT_KEY_LENGTH)
+                    return Key_Provider_Access;
+   function Create (Password : in Ada.Streams.Stream_Element_Array)
+                    return Key_Provider_Access;
 
 end Keystore.Passwords.Keys;
