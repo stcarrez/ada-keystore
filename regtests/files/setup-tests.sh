3bab9c7b3c0452968df9c06b0aae86a8a7cace0d
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-12-21 23:06:46 +0100

    Add --no-secmem-warnings in case there is no secure memory when running gpg (we don't care for the tests)

--- regtests/files/setup-tests.sh
+++ regtests/files/setup-tests.sh
@@ -13,27 +13,27 @@ for USER in $USERS; do
   chmod 700 regtests/files/gnupg/$USER
   echo "Creating GPG user $USER"
   echo "-----------------------"
-  $GPG --homedir=$ROOT/$USER --quiet \
+  $GPG --no-secmem-warning --homedir=$ROOT/$USER --quiet \
        --batch --gen-key regtests/files/$USER-key-script.gpg
-  $GPG --homedir=$ROOT/$USER --quiet \
+  $GPG --no-secmem-warning --homedir=$ROOT/$USER --quiet \
        --armor --export akt-$USER@ada-unit-test.org > $ROOT/$USER.asc
-  $GPG --homedir=$ROOT/$USER --quiet \
+  $GPG --no-secmem-warning --homedir=$ROOT/$USER --quiet \
        --export-ownertrust >> $ROOT/ownertrust.asc
 
   # Generate an AKT configuration file for each user
   cat <<EOF > $ROOT/$USER-akt.properties
-gpg-encrypt=$GPG --homedir=$ROOT/$USER --encrypt --batch --yes --quiet -r \$USER
-gpg-decrypt=$GPG --homedir=$ROOT/$USER --decrypt --batch --yes --quiet
-gpg-list-keys=$GPG --homedir=$ROOT/$USER --list-secret-keys --with-colons --with-fingerprint
+gpg-encrypt=$GPG --no-secmem-warning --homedir=$ROOT/$USER --encrypt --batch --yes --quiet -r \$USER
+gpg-decrypt=$GPG --no-secmem-warning --homedir=$ROOT/$USER --decrypt --batch --yes --quiet
+gpg-list-keys=$GPG --no-secmem-warning --homedir=$ROOT/$USER --list-secret-keys --with-colons --with-fingerprint
 EOF
 done
 
 # Import GPG public keys for user1 so that he knows user2 and user3.
 # user2 and user3 don't know each other.
-$GPG --homedir=`pwd`/regtests/files/gnupg/user1 --quiet \
+$GPG --no-secmem-warning --homedir=`pwd`/regtests/files/gnupg/user1 --quiet \
      --import regtests/files/gnupg/user2.asc
-$GPG --homedir=`pwd`/regtests/files/gnupg/user1 --quiet \
+$GPG --no-secmem-warning --homedir=`pwd`/regtests/files/gnupg/user1 --quiet \
      --import regtests/files/gnupg/user3.asc
-$GPG --homedir=`pwd`/regtests/files/gnupg/user1 --quiet \
+$GPG --no-secmem-warning --homedir=`pwd`/regtests/files/gnupg/user1 --quiet \
      --import-ownertrust regtests/files/gnupg/ownertrust.asc
 
