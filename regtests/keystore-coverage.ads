107f3b344f589165dd9d2cb8ddf5ce3be0622d6a
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-08 18:22:50 +0100

    Stupid test for coverage

---
+++ regtests/keystore-coverage.ads
@@ -0,0 +1,28 @@
+-----------------------------------------------------------------------
+--  keystore-coverage -- Specific tests for coverage
+--  Copyright (C) 2020 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Util.Tests;
+private package Keystore.Coverage is
+
+   procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite);
+
+   type Test is new Util.Tests.Test with null record;
+
+   procedure Test_Deep_Coverage (T : in out Test);
+
+end Keystore.Coverage;
