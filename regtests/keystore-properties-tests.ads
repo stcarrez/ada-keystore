611c85055b707c0da7bacbfe5cd033b58ace03a1
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-12-26 21:49:23 +0100

    Declare the Test_Iterate procedure

--- regtests/keystore-properties-tests.ads
+++ regtests/keystore-properties-tests.ads
@@ -30,4 +30,7 @@ package Keystore.Properties.Tests is
    --  Test the accessing the keystore through property manager.
    procedure Test_Properties (T : in out Test);
 
+   --  Test iterating over the property manager.
+   procedure Test_Iterate (T : in out Test);
+
 end Keystore.Properties.Tests;
