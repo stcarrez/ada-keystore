9deb15a55997c99d2791fcdf8d3a6a52377471a4
Stephane Carrez <Stephane.Carrez@gmail.com>
2023-04-23 11:51:40 +0200

    Cleanup and simplify the coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 NAME=keystore.cov
-lcov --quiet --base-directory . --directory . -c -o $NAME
-lcov --quiet --remove $NAME "/usr*" -o $NAME
-lcov --quiet --remove $NAME "/build*" -o $NAME
-lcov --quiet --remove $NAME "/opt*" -o $NAME
-lcov --quiet --remove $NAME "*/regtests*" -o $NAME
-lcov --quiet --remove $NAME "*/adainclude*" -o $NAME
-lcov --quiet --remove $NAME ada-keystore/b__keystore_harness.adb -o $NAME
-lcov --quiet --remove $NAME ada-keystore/b__akt-main.adb -o $NAME
-lcov --quiet --remove $NAME "*/b__keystore_harness.adb" -o $NAME
-lcov --quiet --remove $NAME "*/b__akt-main.adb" -o $NAME
+lcov --quiet --base-directory . --directory . \
+   --no-external \
+   --exclude '*/b__*.adb' \
+   --exclude '*/regtests*' \
+   --exclude '*/ada-util/*' \
+   -c -o $NAME
 rm -rf cover
 genhtml --quiet -o ./cover -t "test coverage" --num-spaces 4 $NAME
  
